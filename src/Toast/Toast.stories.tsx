9380d6cb8bc2c35c969e988c126cd54a86f10d20
Michael Gee <michaelgee221@gmail.com>
2024-06-18 22:42:27 -0400

    Toast/Sonner component, stories, and dependency additions

---
+++ src/Toast/Toast.stories.tsx
@@ -0,0 +1,66 @@
+import { Toaster, toast } from './';
+import { Button } from '../Button';
+import mdx from './Toast.mdx';
+import { withDarkMode, darkModeParams } from '../_internal/storybook-utils';
+
+const meta = {
+  title: 'Toast',
+  component: Toaster,
+  parameters: {
+    docs: {
+      page: mdx
+    },
+    backgrounds: { disable: true },
+    layout: 'centered'
+  }
+};
+
+export default meta;
+
+export const Default = () => {
+  return (
+    <>
+      <Toaster />
+
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast('Event has been created', {
+            description: 'Sunday, December 03, 2023 at 9:00 AM',
+            action: {
+              label: 'Undo',
+              onClick: () => console.log('Undo')
+            }
+          })
+        }
+      >
+        Show Toast
+      </Button>
+    </>
+  );
+};
+
+export const DarkMode = () => {
+  return (
+    <>
+      <Toaster theme="dark" />
+
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast('Event has been created', {
+            description: 'Sunday, December 03, 2023 at 9:00 AM',
+            action: {
+              label: 'Undo',
+              onClick: () => console.log('Undo')
+            }
+          })
+        }
+      >
+        Show Toast
+      </Button>
+    </>
+  );
+};
+DarkMode.parameters = darkModeParams;
+DarkMode.decorators = [withDarkMode];
--- src/index.ts
+++ src/index.ts
@@ -31,5 +31,6 @@ export * from './Slider';
 export * from './Switch';
 export * from './Tabs';
 export * from './Textarea';
+export * from './Toast';
 
 export * from './utils';
