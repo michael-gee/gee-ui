2418c1548fa01aa538d347015e1fdda8b876c78c
Michael Gee <michaelgee221@gmail.com>
2024-08-14 08:54:47 -0400

    add 'Installation' docs page

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -13,7 +13,7 @@ const preview: Preview = {
     options: {
       storySort: {
         method: 'alphabetical',
-        order: ['Introduction', 'Getting Started', '*']
+        order: ['Introduction', 'Installation', '*']
       }
     }
   }
