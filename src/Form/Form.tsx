3f33afc6e3903ff9814b319ce508b51f0970233e
Michael Gee <michaelgee221@gmail.com>
2025-03-18 10:50:48 -0400

    redeclare FormProvider in Form component to prevent breaking builds and make react-hook-form an optional peerDependency

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -3,14 +3,14 @@
 import * as React from 'react';
 import * as LabelPrimitive from '@radix-ui/react-label';
 import { Slot } from '@radix-ui/react-slot';
-import { Controller, FormProvider, useFormContext } from 'react-hook-form';
+import { Controller, FormProvider as HookFormProvider, useFormContext } from 'react-hook-form';
 import { Label } from '../Label';
 
 import { cn } from '../utils';
 
 import type { ControllerProps, FieldPath, FieldValues } from 'react-hook-form';
 
-const Form = FormProvider;
+const Form = HookFormProvider;
 
 type FormFieldContextValue<
   TFieldValues extends FieldValues = FieldValues,
